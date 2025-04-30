652a915c8093356284c2af1fa86cd4b1807ad722
Baltharaaz <bjbruins1@yahoo.com>
2025-04-29 20:07:56 -0700

    Bugfix function

--- process.py
+++ process.py
@@ -9,15 +9,19 @@ def pandas_to_numpy(df):
     df = pd.DataFrame(df)
     return df.to_numpy()
 
+
 def process(path):
     df = pd.read_csv(path)
     pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
     df = df.dropna(axis=1, how='all') # Dropping column #32 that is a column of NaNs
-    df = df.drop(["id"]) # Redundant ID column
+    df = df.drop(columns=["id"]) # Redundant ID column
     df.diagnosis = [1 if each == "M" else 0 for each in df.diagnosis]
+    df.isnull().sum()
+    df.value_counts()
     true = df["diagnosis"].to_numpy()
     input = df.drop(["diagnosis"], axis=1)
-    input_normalized = (input - np.min(input)) / (np.max(input) - np.min(input)).to_numpy()
+    input_normalized = (input - np.min(input)) / (np.max(input) - np.min(input))
     input_train, input_test, true_train, true_test = train_test_split(input_normalized, true, test_size=0.2, random_state=42)
 
 
